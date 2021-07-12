220d7531cac1652851059eb9a4dfc5fdea390312
vardhan <harshwardhankalse03@gmail.com>
2021-07-12 19:24:15 +0530

    songlist component

---
+++ src/Hooks/components/SongList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SongList = () => {
+  return (
+    <ul>
+      <li>Song 1</li>
+      <li>Song 2</li>
+      <li>Song 3</li>
+    </ul>
+  );
+};
+
+export default SongList;
---
+++ src/Hooks/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Index = () => {
+  return <div className="hooks_app"></div>;
+};
+
+export default Index;
